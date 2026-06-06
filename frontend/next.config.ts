82a9a552b06d2e6b70d974d31ecf9be24fa3eaef
Seyda <seyda.rivasher@gmail.com>
2026-06-05 23:28:22 -0500

    .

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',       // Esto convierte tu proyecto en archivos HTML/CSS/JS puros
+  images: { 
+    unoptimized: true     // Necesario porque el optimizador de imágenes de Next requiere un servidor Node.js
+  },
 };
 
 export default nextConfig;
